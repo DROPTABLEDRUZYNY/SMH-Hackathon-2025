d2b23ca9f1807538d301c0a1a54857446d585a1b
Kryspin Kucha <50886736+blackcaer@users.noreply.github.com>
2025-03-18 23:55:47 +0100

    style: Code cleanup & renaming

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -9,19 +9,17 @@ from rest_framework_simplejwt.views import (
 from . import views
 
 router = DefaultRouter()
-router.register(r'products1', views.ProductItemsView, basename='products1')
-router.register(r'products2', views.ProductViewSet, basename='products2')
-router.register(r'products3', views.ProductReadOnlyViewSet, basename='products3')
+router.register(r"products1", views.ProductItemsView, basename="products1")
+router.register(r"products2", views.ProductViewSet, basename="products2")
+router.register(r"products3", views.ProductReadOnlyViewSet, basename="products3")
 
 urlpatterns = [
-    path('', include(router.urls)),
+    path("", include(router.urls)),
+    path("token/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
+    path("token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
+    path("token/verify/", TokenVerifyView.as_view(), name="token_verify"),
     
-    path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
-    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
-    path('token/verify/', TokenVerifyView.as_view(), name='token_verify'),
-
-    path('apihome/', views.api_home), # localhost:8000/api/
-    #path('products/<int:pk>/', views.ProductDetailView.as_view(), name='product-detail'),
-    #path('v2/products/<int:pk>/', views.ProductDetailView2.as_view({'get': 'retrieve'}), name='product-detail'),
-
+    path("random/", views.get_random_product), 
+    # path('products/<int:pk>/', views.ProductDetailView.as_view(), name='product-detail'),
+    # path('v2/products/<int:pk>/', views.ProductDetailView2.as_view({'get': 'retrieve'}), name='product-detail'),
 ]
--- backend/api/views.py
+++ backend/api/views.py
@@ -33,7 +33,7 @@ User = get_user_model()
 
 
 @api_view(["GET"])
-def api_home(request):
+def get_random_product(request):
     serializer = ProductSerializer(Product.objects.order_by("?").first(), many=False)
     return Response(serializer.data)
 
@@ -49,21 +49,26 @@ class ProductDetailView(RetrieveAPIView):
     #     return Response(serializer.data)
 
 
-class ProductItemsView(ListModelMixin,RetrieveModelMixin, GenericViewSet):
+class ProductItemsView(ListModelMixin, RetrieveModelMixin, GenericViewSet):
     permission_classes = []
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
 
-    @action(detail=True, methods=['get'], authentication_classes=[], permission_classes=[])
-    def is_registered(self, request,pk):  # Placeholder action
-        return Response({'message': f'registered {pk}'})
-    
-class ProductViewSet(ModelViewSet):
-    permission_classes = []
-    queryset = Product.objects.all()
-    serializer_class = ProductSerializer
-    
+    @action(
+        detail=True, methods=["get"], authentication_classes=[], permission_classes=[]
+    )
+    def is_registered(self, request, pk):  # Placeholder action
+        return Response({"message": f"registered {pk}"})
+
+
 class ProductReadOnlyViewSet(ReadOnlyModelViewSet):
     permission_classes = []
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
+
+
+class ProductViewSet(ModelViewSet):
+    permission_classes = []
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+    # http_method_names = ["get", "post", "put", "patch", "delete", "head", "options", "trace"] # Default
