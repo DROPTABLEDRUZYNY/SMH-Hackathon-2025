55f2c34cdcf40c256f34b06b224c642b7e203eae
Kryspin Kucha <50886736+blackcaer@users.noreply.github.com>
2025-03-22 18:30:06 +0100

    feat: Add user registration endpoint and serializer; Update user viewset to handle user registration

--- backend/users/serializers.py
+++ backend/users/serializers.py
@@ -1,6 +1,9 @@
 from rest_framework import serializers
+from django.contrib.auth import get_user_model
 
-from .models import User
+# from .models import User
+
+User = get_user_model()
 
 
 class UserSerializer(serializers.ModelSerializer):
@@ -16,12 +19,24 @@ class UserSerializer(serializers.ModelSerializer):
 
     def validate_first_name(self, value):
         """Ensure the first name contains only letters."""
-        #if not value.isalpha():
+        # if not value.isalpha():
         #    raise serializers.ValidationError("First name must contain only letters.")
         return value.lower().capitalize()
 
     def validate_last_name(self, value):
         """Ensure the last name contains only letters."""
-        #if not value.isalpha():
+        # if not value.isalpha():
         #    raise serializers.ValidationError("Last name must contain only letters.")
         return value.lower().capitalize()
+
+
+class UserRegistrationSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(write_only=True)
+
+    class Meta:
+        model = User
+        fields = ["id", "first_name", "last_name", "email", "birth_date", "password"]
+
+    def create(self, validated_data):
+        user = User.objects.create_user(**validated_data)
+        return user
--- backend/users/views.py
+++ backend/users/views.py
@@ -1,10 +1,13 @@
+from rest_framework.decorators import action
 from django.contrib.auth import get_user_model
+from rest_framework import status
 from rest_framework.generics import RetrieveAPIView
 from rest_framework.mixins import RetrieveModelMixin
-from rest_framework.permissions import IsAuthenticated
+from rest_framework.permissions import AllowAny, IsAuthenticated
+from rest_framework.response import Response
 from rest_framework.viewsets import GenericViewSet, ReadOnlyModelViewSet
 
-from users.serializers import UserSerializer
+from users.serializers import UserRegistrationSerializer, UserSerializer
 
 from django.http import JsonResponse
 
@@ -37,8 +40,22 @@ class UserViewSet(ListModelMixin, RetrieveModelMixin, GenericViewSet):
     queryset = User.objects.all()
     serializer_class = UserSerializer
 
+    @action(detail=False, methods=["post"], authentication_classes=[], permission_classes=[AllowAny],serializer_class=UserRegistrationSerializer)
+    def register(self, request):
+        if request.user.is_authenticated:
+            return JsonResponse(
+                {"error": "Authenticated users cannot register new users."},
+                status=403,
+            )
+            
+        serializer = UserRegistrationSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
-class RetrieveCurrentUserView(RetrieveAPIView):
+class RetrieveUpdateCurrentUserView(RetrieveAPIView, UpdateAPIView):
+    # Using APIViews instead of mixins because of automatic method binding
     serializer_class = UserSerializer
     permission_classes = [IsAuthenticated]
 
