cc039e2beafc02cbb70c4a35cbab972cfeb7812b
Kryspin Kucha <50886736+blackcaer@users.noreply.github.com>
2025-03-23 15:20:51 +0100

    feat: Started building template for frontend; Moving old components to Tailwind 4.x; Installed some of the required components (others require futher verification); Adjusting globals; Fixed color format for charts (removed hsl); Adjusted main page;

---
+++ frontend/app/(mainapp)/(navigation)/layout.tsx
@@ -0,0 +1,33 @@
+import type { Metadata } from "next";
+import '@/app/globals.css';
+import { inter } from '@/app/ui/fonts';
+
+import localFont from "next/font/local";
+import SideNav from "@/app/ui/sidenav";
+
+
+export const metadata: Metadata = {
+  title: {
+    template: '%s | LiveActive',
+    default: 'LiveActive',
+  },
+  description: 'Example site called LiveActive',
+};
+
+export default function Layout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+
+  return  (
+    <span lang='en' suppressHydrationWarning>
+    <div className="flex h-screen flex-col md:flex-row md:overflow-hidden">
+      <div className="w-full flex-none md:w-64">
+        <SideNav />
+      </div>
+      <div className="flex-grow p-6 md:overflow-y-auto md:p-10">{children}</div>
+    </div>
+    </span>
+  );
+}
---
+++ frontend/app/ui/stats/chart-bar-1.tsx
@@ -0,0 +1,63 @@
+"use client"
+
+import { Bar, BarChart, CartesianGrid, XAxis, YAxis } from "recharts"
+
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card"
+import {
+  ChartConfig,
+  ChartContainer,
+  ChartTooltip,
+  ChartTooltipContent,
+} from "@/components/ui/chart"
+
+interface ChartBar1Props {
+  data: { day: string; sales: number; price: number }[];
+  config: ChartConfig;
+}
+
+export default function ChartBar1({ data, config }: ChartBar1Props) {
+  const barChartConfig = {
+    sales: {
+      label: "Activity minutes",
+      color: "var(--chart-1)",
+    },
+    price: {
+      label: "Average activity",
+      color: "var(--chart-2)",
+    },
+  };
+
+  return (
+    <Card>
+      <CardHeader>
+        <CardTitle>Your activity during this week</CardTitle>
+        <CardDescription>Daily activity minutes</CardDescription>
+      </CardHeader>
+      <CardContent>
+        <ChartContainer config={barChartConfig}>
+          <BarChart
+            data={data}
+            margin={{
+              top: 5,
+              right: 30,
+              left: 20,
+              bottom: 5,
+            }}
+          >
+            <CartesianGrid strokeDasharray="3 3" />
+            <XAxis dataKey="day" />
+            <YAxis />
+            <ChartTooltip content={<ChartTooltipContent />} />
+            <Bar dataKey="sales" fill="var(--color-sales)" />
+          </BarChart>
+        </ChartContainer>
+      </CardContent>
+    </Card>
+  )
+}
