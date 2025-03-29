6a8b8f4512fe763d4843f681e31739a596d22834
Yehor Kharchenko <yehorkharchenko4@gmail.com>
2025-03-29 11:31:21 +0100

    starter page

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -23,16 +23,12 @@ export const metadata: Metadata = {
 
 export default function RootLayout({
   children,
-}: Readonly<{
+}: {
   children: React.ReactNode;
-}>) {
+}) {
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        {children}
-      </body>
+      <body className="bg-black text-white">{children}</body>
     </html>
   );
 }
