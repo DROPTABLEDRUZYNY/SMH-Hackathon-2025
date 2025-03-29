491100f4315aca4f6cb53f2b544c7f85899e8a71
Yehor Kharchenko <yehorkharchenko4@gmail.com>
2025-03-29 13:57:55 +0100

    created routes

---
+++ frontend/app/(mainapp)/(navigation)/mark-trash/page.tsx
@@ -0,0 +1,10 @@
+import { ProfileForm } from "@/app/ui/contact/contactForm";
+import FullScreenMap from "@/components/ui/map";
+
+export default function Page() {
+  return (
+    <div>
+      <FullScreenMap />
+    </div>
+  );
+}
